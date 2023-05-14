8680bea1b1b722fbe681104f67d69a5e4119107e
parshuram kalunkhe <107802301+parshuramkalunkhe@users.noreply.github.com>
2023-05-14 17:16:13 +0530

    Add files via upload

--- config.h
+++ config.h
@@ -2,14 +2,13 @@
 static const Block blocks[] = {
 
 /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-  {"",  "cpu", 	5, 2},
-  {"", 	"memory", 1, 2},
-  {"",  "nettraf",  2,  15}, 
-  {"",  "volume", 1,  10},
-  {"",  "clock",  30, 1},
+  {"",  "cpu", 	10, 18},
+  {"", 	"memory", 10, 14},
+  {"",  "nettraf",  1,  16}, 
   {"",  "internet", 5,  4},
-  {"",  "battery", 60, 4},
-  {"", "", 1, 1},
+  {"",  "volume", 1,  10},
+  {"",  "battery", 5, 3},
+  {"",  "clock",  60, 1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
